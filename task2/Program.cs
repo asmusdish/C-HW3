23093c89afe2d3ba5714f814c05fdc73f59699f4
Denis <asmusdish@ya.ru>
2023-01-25 18:39:34 +0500

    add tasks

---
+++ task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int xa, ya, za, xb, yb, zb;
+double distant(double xa, double ya, double za, double xb, double yb, double zb)
+{
+   return Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
+}
+
+Console.WriteLine("Программа для вычисления расстояния между точками");
+
+Console.WriteLine("Введите координату Х точки А: ");
+while (!int.TryParse(Console.ReadLine(), out xa))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Y точки А: ");
+while (!int.TryParse(Console.ReadLine(), out ya))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Z точки А: ");
+while (!int.TryParse(Console.ReadLine(), out za))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Х точки B: ");
+while (!int.TryParse(Console.ReadLine(), out xb))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Y точки B: ");
+while (!int.TryParse(Console.ReadLine(), out yb))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Z точки B: ");
+while (!int.TryParse(Console.ReadLine(), out zb))
+{
+Console.Write("Введен неверный параметр");
+}
+
+double res = distant(xa, ya, za, xb, yb, zb);
+
+Console.WriteLine(res.ToString("#.##"));
---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Вычисление кубов значений чисел от 1 до N");
+Console.WriteLine("Введите число N: ");
+if (!int.TryParse(Console.ReadLine(), out int N) || N <= 0) {
+Console.WriteLine("Введено не целое или отрицательное число");
+return;
+}
+
+void func(int N) {
+    int count = 1;
+    Console.Write((int) Math.Pow(count, 3));
+    count++;
+    while(count <= N) {
+        Console.Write(", " + (int) Math.Pow(count, 3));
+        count++;
+    }
+}
+
+func(N);
